4a0180e14f8a5c5a7ecb61f0ac4d87ea1e51acac
Toxiusus <icek1lla2323@gmail.com>
2024-01-31 15:50:46 +0300

    added range-slider

--- src/js/range-slider.js
+++ src/js/range-slider.js
@@ -1,38 +1,38 @@
-const rangeInput = document.querySelectorAll(".range-inputs__num");
-const priceInput = document.querySelectorAll(".range-slider__input");
-const progress = document.querySelector(".slider__progres");
+let rangeInput = document.querySelectorAll(".range-input input")
+let priceInput = document.querySelectorAll(".price-input input")
+let progress = document.querySelector(".slider .progress")
 
 let priceGap = 1000;
 
 priceInput.forEach(input => {
-  input.addEventListener("input", e => {
-    let minVal = parseInt(priceInput[0].value);
-    let maxVal = parseInt(priceInput[1].value);
+  input.addEventListener('click', e => {
+    let minVal = parseInt(priceInput[0].value)
+    let maxVal = parseInt(priceInput[1].value)
 
-    if((maxVal - minVal >= priceGap) && maxVal <= 10000){
-      if(e.target.classList === "input-min"){
+    if ((maxVal - minVal >= priceGap) && maxVal <= 10000) {
+      if (e.target.className === "input-min") {
         rangeInput[0].value = minVal;
         progress.style.left = (minVal / rangeInput[0].max) * 100 + "%";
-      }else {
+      }else{
         rangeInput[1].value = maxVal;
         progress.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
       }
-    }
+    } 
   });
 });
 
 rangeInput.forEach(input => {
-  input.addEventListener("input", e => {
-    let minVal = parseInt(rangeInput[0].value);
-    let maxVal = parseInt(rangeInput[1].value);
+  input.addEventListener('click', e => {
+    let minVal = parseInt(rangeInput[0].value)
+    let maxVal = parseInt(rangeInput[1].value)
 
-    if(maxVal - minVal < priceGap){
-      if(e.target.classList === "range-min"){
+    if (maxVal - minVal < priceGap) {
+      if (e.target.className === "range-min") {
         rangeInput[0].value = maxVal - priceGap;
-      }else {
+      }else{
         rangeInput[1].value = minVal + priceGap;
       }
-    }else {
+    } else {
       priceInput[0].value = minVal;
       priceInput[1].value = maxVal;
       progress.style.left = (minVal / rangeInput[0].max) * 100 + "%";
